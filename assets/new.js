d0994bfb3bb1e9ee47ee534872c321aa54d07796
metti <mehdiasghari615@gmail.com>
2023-08-14 22:51:22 +0330

    obb

--- assets/new.js
+++ assets/new.js
@@ -1,12 +1,8 @@
-// Example 1: Assigning an anonymous function to a variable
-const greet = function(name) {
-    console.log(`Hello, ${name}!`);
-  };
-  
-  greet("John"); // Output: Hello, John!
-  
-  // Example 2: Using an anonymous function as a callback
-  setTimeout(function() {
-    console.log("This message will be displayed after 3 seconds.");
-  }, 3000);
+var person= {  //object
+    name: "alex",
+    family:"cezar",
+    age:45,
+    address:"usa"
+}
+console.log(person);
 
