7cfe9c1f690082e4a0df2eb5c30374b482cbe41f
metti <mehdiasghari615@gmail.com>
2023-08-14 22:47:24 +0330

    assist code

--- assets/new.js
+++ assets/new.js
@@ -1,12 +1,12 @@
-function findBiggesFraction()  //fuction named
-
-{  
-a<b ? console.log("a"+a) : console.log("b"+b);
-
-}
-
-var a=5/8;
-var b=8/3;
-
-findBiggesFraction();
+// Example 1: Assigning an anonymous function to a variable
+const greet = function(name) {
+    console.log(`Hello, ${name}!`);
+  };
+  
+  greet("John"); // Output: Hello, John!
+  
+  // Example 2: Using an anonymous function as a callback
+  setTimeout(function() {
+    console.log("This message will be displayed after 3 seconds.");
+  }, 3000);
 
