a2f447adde271a4a85630449fc822aff8f90155e
metti <mehdiasghari615@gmail.com>
2023-08-15 14:42:57 +0330

    obb con

--- assets/new.js
+++ assets/new.js
@@ -1,12 +1,11 @@
-var course={
-    title:"had",
-    age:45,
-    time:1,
-    views:0,
-    updateview:function () {    //anonymous f
-        return ++course.views;   //1 ezaf per view 
-    }
+function Benz(model,color,year,type){   //object constructor
+    this.model=model,
+    this.color=color,
+    this.year=year,
+    this.type=type;
 }
-course.updateview();      //farakhani in brye view 
 
-console.log(course);
+var Benz2=new Benz("gclass","red",2010,"sedan");
+var Benz3=new Benz("sls","blue",2021,"sport");
+console.log(Benz3);
+console.log(Benz2);
